dd21497e308ceb28bbd13cd65129644171a0b5f0
Jędrzej Bogumił Lewandowski, MD <jedrzejblew@gmail.com>
2022-05-22 17:46:06 +0200

    refactor: move current folders to old

--- scripts/create-migration.sh
+++
@@ -1,6 +0,0 @@
-#/usr/bin/env bash
-export MIGRATION_NAME=$1 
-docker-compose \
-    -f docker-compose.yml -f docker-compose.cli.yml build create_migration && \
-docker-compose \
-    -f docker-compose.yml -f docker-compose.cli.yml up create_migration
--- scripts/generate-migration.sh
+++
@@ -1,6 +0,0 @@
-#/usr/bin/env bash
-export MIGRATION_NAME=$1 
-docker-compose \
-    -f docker-compose.yml -f docker-compose.cli.yml build generate_migration && \
-docker-compose \
-    -f docker-compose.yml -f docker-compose.cli.yml up generate_migration
