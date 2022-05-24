590dd38c620c9cca309daefe687608eabe65f3d1
Jędrzej Bogumił Lewandowski, MD <jedrzejblew@gmail.com>
2022-05-24 19:24:24 +0200

    feat: static assets

--- app/root.tsx
+++ app/root.tsx
@@ -2,8 +2,8 @@ import type {
   LinksFunction,
   LoaderFunction,
   MetaFunction,
-} from "@remix-run/node";
-import { json } from "@remix-run/node";
+} from "@remix-run/node"
+import { json } from "@remix-run/node"
 import {
   Links,
   LiveReload,
@@ -11,30 +11,39 @@ import {
   Outlet,
   Scripts,
   ScrollRestoration,
-} from "@remix-run/react";
+} from "@remix-run/react"
 
-import tailwindStylesheetUrl from "./styles/tailwind.css";
-import { getUser } from "./session.server";
+import tailwindStylesheetUrl from "./styles/tailwind.css"
+import { getUser } from "./session.server"
 
 export const links: LinksFunction = () => {
-  return [{ rel: "stylesheet", href: tailwindStylesheetUrl }];
-};
+  return [{ rel: "stylesheet", href: tailwindStylesheetUrl }]
+}
 
 export const meta: MetaFunction = () => ({
   charset: "utf-8",
-  title: "Remix Notes",
+  title: "Prioratus: life in the rhythm of breviary",
   viewport: "width=device-width,initial-scale=1",
-});
+  icon: "/favicon.ico",
+  description: "Life in the rhythm of breviary",
+  manifest: "/manifest.json",
+  "theme-color": "#565676",
+  "apple-touch-icon": "/apple-touch-icon.png",
+  "apple-touch-startup-image": "/launch.png",
+  "apple-mobile-web-app-title": "Prioratus",
+  "apple-mobile-web-app-capable": "yes",
+  "apple-mobile-web-app-status-bar-style": "black"
+})
 
 type LoaderData = {
-  user: Awaited<ReturnType<typeof getUser>>;
-};
+  user: Awaited<ReturnType<typeof getUser>>
+}
 
 export const loader: LoaderFunction = async ({ request }) => {
   return json<LoaderData>({
     user: await getUser(request),
-  });
-};
+  })
+}
 
 export default function App() {
   return (
@@ -50,5 +59,5 @@ export default function App() {
         <LiveReload />
       </body>
     </html>
-  );
+  )
 }
